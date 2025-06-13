a371a6f266335d876c76bef0079585d099873b84
Felix Koppe <fkoppe@web.de>
2025-06-13 20:16:57 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -18,7 +18,7 @@ sudo pacman -S --needed --noconfirm lldb cuda vulkan-headers nvidia-container-to
 sudo pacman -S --needed --noconfirm docker docker-compose
 
 # other
-sudo pacman -S --needed --noconfirm keepassxc chromium inkscape blender okular elisa vlc krita obsidian
+sudo pacman -S --needed --noconfirm keepassxc chromium inkscape blender okular elisa vlc krita obsidian libreoffice-still
 sudo pacman -S --needed --noconfirm audacity
 
 # game
