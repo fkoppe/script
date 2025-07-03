2466a9fb1748ec68e45b509503ec920bbd596b5e
Felix Koppe <fkoppe@web.de>
2025-07-03 17:43:46 +0200

    Update aur.sh

--- setup/aur.sh
+++ setup/aur.sh
@@ -3,10 +3,17 @@ sudo -v
 while true; do sudo -n true; sleep 60; done 2>/dev/null &
 KEEP_ALIVE_PID=$!
 
-paru -S --needed --noconfirm --skipreview tidal-hifi-bin visual-studio-code-bin 64gram-desktop-bin backintime vesktop-bin drawio-desktop proton-mail-bin
+# communication
+paru -S --needed --noconfirm --skipreview proton-mail-bin vesktop-bin 64gram-desktop-bin
+
+# creative
+paru -S --needed --noconfirm --skipreview visual-studio-code-bin drawio-desktop onlyoffice-bin
 
 # browser
 paru -S --needed --noconfirm --skipreview librewolf-bin brave-bin 
 
 # printer
 paru -S --needed --noconfirm --skipreview epson-inkjet-printer-escpr-bin epson-inkjet-printer-escpr2-bin
+
+# other
+paru -S --needed --noconfirm --skipreview tidal-hifi-bin backintime
