84ebc4b8e0f5cc2deb282304ac0488c536605f24
Felix Koppe <fkoppe@web.de>
2026-01-19 08:48:04 +0100

    Work

--- jupyter_lab.sh
+++ jupyter_lab.sh
@@ -3,4 +3,4 @@ sudo -v
 while true; do sudo -n true; sleep 60; done 2>/dev/null &
 KEEP_ALIVE_PID=$!
 
-sudo paru -S --needed --noconfirm jupyterlab cling-jupyter-git julia python-ipykernel evcxr_jupyter jupyter-octave_kernel
+paru -S --needed --noconfirm --skipreview jupyterlab cling-jupyter-git julia python-ipykernel evcxr_jupyter jupyter-octave_kernel
